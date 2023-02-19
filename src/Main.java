d6dfe82941744200a0ca7833a169cee861fb3e71
ulyana <bsergeybabak2000@gmail.com>
2023-02-19 18:28:04 +0300

    дз

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 () {
+    int pinepple = 1;
+    System.out.println("Значение переменной pinepple с типом инт равно "+ pinepple);
+    byte apple = 120;
+    System.out.println("Значение переменной apple с типом byte равно " + apple);
+    short orange = 25;
+    System.out.println("Значение переменной orange с типом short равно " + orange);
+    long bananas = 2038;
+    System.out.println("Значение переменной bananas с типом long равно " + bananas);
+    float melon = 50f;
+    System.out.println("Значение переменной melon с типом float равно " + melon);
+    double lemon = 2.35;
+    System.out.println("Значение переменной lemon с типом double равно " + lemon);
+
+    }
+
+    public static void task2 () {
+
+    }
+}
